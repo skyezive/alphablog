0e7bedba94b007f13c7cd10bc289f7f6893b40af
skyezive <89458938+skyezive@users.noreply.github.com>
2023-10-19 13:32:55 +0200

    Added Login and Logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-root 'pages#home'
-get 'about', to: 'pages#about'
-resources :articles
-get 'signup', to: 'users#new'
-post 'users', to: 'users#create'
-resources :users, except: [:new]
+  root 'pages#home'
+  get 'about', to: 'pages#about'
+  resources :articles
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
