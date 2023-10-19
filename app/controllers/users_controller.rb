54f59b39aae4cbe14de68c282b0a7d2ec819e90b
skyezive <89458938+skyezive@users.noreply.github.com>
2023-10-19 14:14:21 +0200

    Authentication Helper Methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
-  def show 
-    @user = User.find(params[:id])
+
+  before_action :set_user, only: [:show, :edit, :update]
+
+  def show
     @articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
 
@@ -15,6 +17,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "Welcome to capy blog #{@user.username}!!! You have successfully signed up!"
       redirect_to articles_path
     else
@@ -23,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:notice] = "Your account information was successfully updated"
       redirect_to @user
@@ -39,6 +40,10 @@ class UsersController < ApplicationController
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
