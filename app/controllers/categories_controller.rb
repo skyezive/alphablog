ab968c17e3ba19418618d8a8ed0c33c754d3c418
skyezive <89458938+skyezive@users.noreply.github.com>
2023-10-26 17:04:59 +0200

    Add categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,12 +1,15 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
   def new
     @category = Category.new
   end
 
   def index
+    @categories = Category.paginate(page: params[:page], per_page: 5)
   end
 
   def show
+    @category = Category.find(params[:id])
   end
 
   def create
@@ -26,6 +29,12 @@ class CategoriesController < ApplicationController
     params.require(:category).permit(:name)
   end
 
-
+  def require_admin
+    if !(logged_in? && current_user.admin?)
+      flash[:alert] = "Only admins can perform that action"
+      redirect_to categories_path
+    end
+    
+  end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_20_120612) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_26_132811) do
+  create_table "article_categories", force: :cascade do |t|
+    t.integer "article_id"
+    t.integer "category_id"
+  end
+
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
